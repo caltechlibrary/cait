1aa46cc1ddf6601b4f4fa066d27c477edb604ff4
R. S. Doiel <rsdoiel@gmail.com>
2016-01-26 15:16:52 -0800

    snapshot

--- scripts-examples/digitalobject.js
+++ scripts-examples/digitalobject.js
@@ -2,12 +2,12 @@
  * This is an example JavaScript file for importing Digital Objects from a
  * Excel spreadsheet with the following columns.
  * + Digital Object ID
- * + Title
+ * + Title (map to title)
  * + Series
  * + Keywords
  * + Name _and Subject
  * + url_online oral history URL
- * + Oral History Text by Item ID::Text Description
+ * + Oral History Text by Item ID::Text Description (map to file_versions)
  * + Oral History Text by Item ID::Search Text
  *
  * These need to map into a JSONModel(:digital_object) in ArchivesSpace.
@@ -69,7 +69,7 @@ login();
 
 function callback(row) {
     //FIXME: need the current date/time in various formats...
-    //FIXME: look up accession that is related
+    //FIXME: look up accession that is related so I can populate linked_instances
     //FIXME: Need to figure out exactly how the row object maps to ArchivesSpace's
     // concept of a Digital Object.
 
@@ -99,6 +99,12 @@ function callback(row) {
         ],
         created_by: apiUsername,
         last_modified_by: apiUsername,
+        user_mtime: "2016-01-26T00:00:00Z",
+        system_mtime: "2016-01-26T00:00:00Z",
+        create_time: "2016-01-26T00:00:00Z",
+        repository: {
+            ref: '/repositories/2'
+        },
         external_documents: [],
         rights_statements: [],
         linked_agents: [],
