cf51c42743316fc4f2b50de6d4a7b919d269826b
R. S. Doiel <rsdoiel@gmail.com>
2016-03-01 11:16:25 -0800

    snapshot

--- examples/caitjs/convert-linked-agents-creators-to-subject.js
+++ examples/caitjs/convert-linked-agents-creators-to-subject.js
@@ -9,6 +9,7 @@ ids = api.listAccessions(2);
 ids.forEach(function (id) {
     var a = api.getAccession(2, id), i = 0, changed = false;
     if (a.linked_agents !== undefined && a.linked_agents.length > 0) {
+        changed = false;
         for (var i = 0; i < a.linked_agents.length; i++) {
             if (a.linked_agents[i].role === "creator") {
                 a.linked_agents[i].role = "subject";
