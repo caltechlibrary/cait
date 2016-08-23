b862f908b7b18121856513b54f81d16e24b4d107
R. S. Doiel <rsdoiel@gmail.com>
2016-08-23 08:06:58 -0700

    Revised script to match current practice
    Production expectations
    + Website in /Sites/$WEBSITE_NAME
    + /etc/cait.bash for configuration
    + $HOME/bin for cait binary installations

--- scripts/nightly-update.sh
+++ scripts/nightly-update.sh
@@ -8,29 +8,28 @@ function consolelog {
 
 # Change directory to where cait is installed
 consolelog  "Running as $USER"
-if [ "$USER" = "root" ]; then
-    cd /archivesspace/cait
-fi
+cd $HOME
+
 consolelog "Working path $(pwd)"
 # Load the cait configuration
-if [ -f etc/setup.conf ]; then
-    consolelog "Configuration $(pwd)/etc/setup.conf"
-    . etc/setup.conf
+if [ -f /etc/cait.bash ]; then
+    consolelog "Configuration /etc/cait.bash"
+    . /etc/cait.bash
 fi
 
 # Export the current content from ArchivesSpace
-./bin/cait archivesspace export
+cait archivesspace export
 # Generate webpages
-./bin/genpages
+genpages
 if [ "$USER" = "root" ]; then
     /etc/init.d/servepages stop
 fi
 # Index webpages
-./bin/indexpages
+indexpages
 
 # You should now be ready to reload the search engine/servepage service
 if [ "$USER" = "root" ]; then
     /etc/init.d/servepages start
 else
-    ./bin/servepages
+    servepages
 fi
