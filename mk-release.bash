5111b75a49e4fa4a5f358b1058f83cae0336e390
R. S. Doiel <rsdoiel@gmail.com>
2016-10-31 17:33:22 -0700

    Added some output for release processing

--- mk-release.bash
+++ mk-release.bash
@@ -5,11 +5,13 @@
 RELEASE_NAME=cait
 
 for PROGNAME in cait genpages indexpages servepages sitemapper xlsximporter; do
+    echo "Compiling $PROGNAME for all architectures"
     env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
     env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/windows-amd64/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
     env CGO_ENABLED=0 GOOS=darwin	GOARCH=amd64 go build -o dist/macosx-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
     env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -o dist/raspberrypi-arm6/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
     env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi-arm7/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
 done
+echo "Zipping release $RELEASE_NAME"
 zip -r $RELEASE_NAME-binary-release.zip README.md INSTALL.md LICENSE dist/*
 
